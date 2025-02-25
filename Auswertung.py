f8e404e99909631fcd90f24865db911435915cd1
Rentheus <axel.andree@gmx.net>
2025-02-25 11:26:25 +0100

    hist for calibrations 2

--- Auswertung.py
+++ Auswertung.py
@@ -33,15 +33,17 @@ data_r_c = [np.mean(data_raum[:,1]),np.mean(data_77K[:,1]), np.mean(data_He[-10:
 sigma_r_c = [np.std(data_raum[:,1],ddof=1),np.std(data_77K[:,1],ddof=1), np.std(data_He[-10:,1],ddof=1)]
 
 fig, ax = fig, ax = plt.subplots(1, 2, figsize=(10,4), layout = "tight")
-ax[0].hist(data_raum[:,0], 5)
+ax[0].hist(data_raum[:,0], 6)
 ax[0].axvline(data_r_pt[0], color = "r", label = "Mean")
 ax[0].axvline(data_r_pt[0]-sigma_r_pt[0], color = "orange", label = "Standard Deviation", ls = ":")
 ax[0].axvline(data_r_pt[0]+sigma_r_pt[0], color = "orange", label = "Standard Deviation", ls = ":")
+ax[0].scatter(data_raum[:,0], np.zeros_like(data_raum[:,0])+ 0.4, color = "darkblue", marker = "x", label = "Measurements" )
 
-ax[1].hist(data_77K[:,0], 5)
+ax[1].hist(data_77K[:,0], 6)
 ax[1].axvline(data_r_pt[1], color = "r", label = "Mean")
 ax[1].axvline(data_r_pt[1]-sigma_r_pt[1], color = "orange", label = "Standard Deviation", ls = ":")
 ax[1].axvline(data_r_pt[1]+sigma_r_pt[1], color = "orange", label = "Standard Deviation", ls = ":")
+ax[1].scatter(data_77K[:,0], np.zeros_like(data_77K[:,0])+ 0.4, color = "darkblue", marker = "x", label = "Measurements" )
 plt.show()
 
 
@@ -51,16 +53,19 @@ ax[0].hist(data_raum[:,1], 8)
 ax[0].axvline(data_r_c[0], color = "r", label = "Mean")
 ax[0].axvline(data_r_c[0]-sigma_r_c[0], color = "orange", label = "Standard Deviation", ls = ":")
 ax[0].axvline(data_r_c[0]+sigma_r_c[0], color = "orange", label = "Standard Deviation", ls = ":")
+ax[0].scatter(data_raum[:,1], np.zeros_like(data_raum[:,1])+ 0.4, color = "darkblue", marker = "x", label = "Measurements" )
 
 ax[1].hist(data_77K[:,1], 8)
 ax[1].axvline(data_r_c[1], color = "r", label = "Mean")
 ax[1].axvline(data_r_c[1]-sigma_r_c[1], color = "orange", label = "Standard Deviation", ls = ":")
 ax[1].axvline(data_r_c[1]+sigma_r_c[1], color = "orange", label = "Standard Deviation", ls = ":")
+ax[1].scatter(data_77K[:,1], np.zeros_like(data_77K[:,1])+ 0.4, color = "darkblue", marker = "x", label = "Measurements" )
 
 ax[2].hist(data_He[-10:,1], 8)
 ax[2].axvline(data_r_c[2], color = "r", label = "Mean")
 ax[2].axvline(data_r_c[2]-sigma_r_c[2], color = "orange", label = "Standard Deviation", ls = ":")
 ax[2].axvline(data_r_c[2]+sigma_r_c[2], color = "orange", label = "Standard Deviation", ls = ":")
+ax[2].scatter(data_He[-10:,1], np.zeros_like(data_He[-10:,1])+ 0.4, color = "darkblue", marker = "x", label = "Measurements" )
 
 
 
